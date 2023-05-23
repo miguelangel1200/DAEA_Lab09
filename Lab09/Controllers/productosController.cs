d417e28d94d809be52b8f0714fd78cb21d6a0592
miguelangel1200 <miguelangel@gmail.com>
2023-05-22 21:42:25 -0500

    first commit

---
+++ Lab09/Controllers/productosController.cs
@@ -0,0 +1,47 @@
+﻿using Lab09.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab09.Controllers
+{
+    public class productosController : Controller
+    {
+
+        private NeptunoEntities db = new NeptunoEntities();
+
+        // GET: productos
+        public ActionResult Index()
+        {
+            return View(db.productos.ToList());
+        }
+
+        // GET: clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: clientes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "")] productos productos)
+        {
+            if (ModelState.IsValid)
+            {
+                int maxId = db.productos.Max(x => x.idproducto); // Obtiene el ID máximo de la tabla productos
+                productos.idproducto = maxId + 1; // Genera un nuevo ID sumando 1 al máximo ID
+
+                db.productos.Add(productos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productos);
+        }
+    }
+}
